240258a4f468e9e907a0a7ede87c26ddf0374e27
sharafutw <sharafudeen@thinkingwealth.ai>
2023-11-07 00:12:45 +0530

    menu widget set

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:st_peters_jacobite_church_flutter/screens/home/widgets/menu_widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          Text("St. Peter's Jacobite Syrian Orthodox Church"),
+          Text('UNDER THE HOLY APOSTOLIC SEE OF ANTIOCH & ALL THE EAST'),
+          Text('KINGDOM OF BAHRAIN'),
+          MenuWidget()
+        ],
       ),
     );
   }
--- lib/theme/assets.dart
+++ lib/theme/assets.dart
@@ -2,6 +2,17 @@ class AppAssets {
   ///[IMAGES]
   static const logoWatermark = 'assets/images/logo_watermark.png';
   static const church = 'assets/images/church.png';
+}
 
-  ///[ICONS]
+///[ICONS]
+class AppIcons {
+  // ICON USED IN HOME SCREEN
+  static const homeAboutChurch = 'assets/home_icons/about_the_church.svg';
+  static const homeAreaUnit = 'assets/home_icons/area_unit.svg';
+  static const homeChurchOfficials = 'assets/home_icons/church_officials.svg';
+  static const homeDownloads = 'assets/home_icons/downloads.svg';
+  static const homeMember = 'assets/home_icons/members.svg';
+  static const homeNewsEvents = 'assets/home_icons/news_events.svg';
+  static const homePrayerTime = 'assets/home_icons/pareyertime.svg';
+  static const homeSpiritual = 'assets/home_icons/sprituals.svg';
 }
