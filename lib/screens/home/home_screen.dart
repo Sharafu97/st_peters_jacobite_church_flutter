cf232b1b87bb125abcb048a6e4966c6c18f5bf1f
sharafutw <sharafudeen@thinkingwealth.ai>
2023-11-07 10:12:55 +0530

    changes

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:st_peters_jacobite_church_flutter/screens/home/widgets/menu_widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,13 +9,27 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [
-          Text("St. Peter's Jacobite Syrian Orthodox Church"),
-          Text('UNDER THE HOLY APOSTOLIC SEE OF ANTIOCH & ALL THE EAST'),
-          Text('KINGDOM OF BAHRAIN'),
-          MenuWidget()
-        ],
+        children: [],
       ),
     );
+  }
+
+  // Widget menuIcons(String icon, String label) {
+  //   return Row(
+  //     children: [
+  //       SvgPicture.asset(icon, height: 24),
+  //       Text(label),
+  //     ],
+  //   );
+  // }
+
+  menuIcons(double angle, {String? icon, String? label}) {
+    final double rad = vector_math_64.radians(angle);
+    return Transform(
+        transform: Matrix4.identity()..translate(cos(rad), sin(rad)),
+        child: IconButton(
+          icon: SvgPicture.asset(icon ?? '', height: 24),
+          onPressed: () {},
+        ));
   }
 }
