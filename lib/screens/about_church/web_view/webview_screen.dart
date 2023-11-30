0ec71037dc10b07fc9ce13260f4ea0f26737d557
Vaisakh <captprice33@gmail.com>
2023-11-30 13:52:19 +0530

    web view corrections

--- lib/screens/about_church/web_view/webview_screen.dart
+++ lib/screens/about_church/web_view/webview_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:st_peters_jacobite_church_flutter/screens/drawer/side_drawer.dart';
 import 'package:st_peters_jacobite_church_flutter/widgets/appbar.dart';
 import 'package:st_peters_jacobite_church_flutter/widgets/contact_bottomsheet.dart';
-import 'package:st_peters_jacobite_church_flutter/widgets/loading_widget.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewScreen extends StatefulWidget {
@@ -17,40 +16,14 @@ class WebViewScreen extends StatefulWidget {
 class _WebViewScreenState extends State<WebViewScreen> {
   static final _drawerKey = GlobalKey<ScaffoldState>();
   late final WebViewController _controller;
-  bool _isLoading = true;
 
   @override
   void initState() {
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      final controller = WebViewController.fromPlatformCreationParams(
-          const PlatformWebViewControllerCreationParams())
-        ..setJavaScriptMode(JavaScriptMode.unrestricted)
-        ..setBackgroundColor(const Color(0x00000000))
-        ..setNavigationDelegate(
-          NavigationDelegate(
-            onProgress: (int progress) {
-              debugPrint('progress: $progress');
-              if (progress == 100 && mounted) {
-                _isLoading = false;
-              }
-            },
-            onPageStarted: (String url) {
-              debugPrint('$url started');
-            },
-            onPageFinished: (String url) {
-              debugPrint('$url loaded');
-            },
-            onWebResourceError: (WebResourceError error) {
-              debugPrint('ERROR: ${error.description}');
-            },
-            onNavigationRequest: (NavigationRequest request) {
-              return NavigationDecision.prevent;
-            },
-          ),
-        )
-        ..loadRequest(Uri.parse(widget.url));
-      _controller = controller;
-    });
+    _controller = WebViewController.fromPlatformCreationParams(
+        const PlatformWebViewControllerCreationParams())
+      ..loadRequest(Uri.parse(widget.url))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000));
 
     super.initState();
   }
@@ -67,11 +40,9 @@ class _WebViewScreenState extends State<WebViewScreen> {
         height: MediaQuery.of(context).size.height -
             const CustomAppbar().preferredSize.height,
         width: MediaQuery.of(context).size.width,
-        child: _isLoading
-            ? const Center(child: LoadingWidget())
-            : WebViewWidget(
-                controller: _controller,
-              ),
+        child: WebViewWidget(
+          controller: _controller,
+        ),
       ),
       bottomSheet: const ContactBottomsheet(),
     );
